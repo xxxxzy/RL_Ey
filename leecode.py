2852f37e0b27fc1f81fa3bf464d98fec8b24215e
xxxxzy <xzyhhhhh@hotmail.com>
2024-02-21 23:58:07 +0800

    0221

--- leecode.py
+++ leecode.py
@@ -26,11 +26,44 @@ class Solution:
         else:
             return False
 
+#14
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        end=0
+        dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        if 'IX' in s:
+            s=s.replace('IX','')   
+            end +=9
+        if 'IV' in s:
+            s=s.replace('IV','')
+            end+=4
+        if 'XL'in s:
+            s=s.replace('XL','')
+            end+=40
+        if 'XC' in s:
+            s=s.replace('XC','')
+            end+=90
+        if 'CD' in s:
+            s=s.replace('CD','')
+            end +=400
+        if 'CM' in s:
+            s=s.replace('CM','')
+            end+=900       
+        for i in s:
+                end+=dic[i]        
+        return end
 '''
-s = '1231I'
-b = str(s)[::-1]
-num = 0
-for i in b:
-    if i is 'I':
-        num = num + 1
-    if i
+#20
+strs = ["flower","flow","flight"]
+minLen = min(len(s) for s in strs)
+strs[1]
+#print(strs[0][:2])
+a = 0
+for i in range(0,minLen-1):
+    for j in range(0,len(strs)-1):
+        print(strs[1])
+        if strs[0][i] != strs[j][i]:
+            a = i
+            print(i)
+            break
+#print(strs[0][:0])
